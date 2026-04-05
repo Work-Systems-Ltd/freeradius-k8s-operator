d831d8e10408abfc3d7b2bd7499181c59ad4b0f8
tbotnz <tonynealon1989@gmail.com>
2026-04-05 00:27:07 +0000

    refactor: move template constants to a new file for better organization

---
+++ internal/renderer/constants.go
@@ -0,0 +1,12 @@
+package renderer
+
+const (
+	tmplRadiusd = "radiusd.conf.tmpl"
+	tmplClients = "clients.conf.tmpl"
+	tmplDefault = "default.tmpl"
+	tmplSQL     = "sql.tmpl"
+	tmplLDAP    = "ldap.tmpl"
+	tmplEAP     = "eap.tmpl"
+	tmplREST    = "rest.tmpl"
+	tmplRedis   = "redis.tmpl"
+)
